46d6f0aa395fd8a66dbf66f5403cf79785de6b8d
pratik-code <dellpratik@gmail.com>
2024-04-10 10:37:21 +0530

    New Repo

--- Meme Generator/script.js
+++ Meme Generator/script.js
@@ -4,10 +4,23 @@ const memeImage = document.querySelector(".meme-generator img");
 const memeTitle = document.querySelector(".meme-generator .meme-title");
 const memeAuthor = document.querySelector(".meme-generator .meme-author");
 
-const generateMeme = ()=> {
-    fetch("https://meme-api.com/gimme").then(resnponse => resnponse.json()).then(data => {
-        updateDetails(data.url, data.title, data.author);
-    });
+const generateMeme = async ()=> {
+
+    // fetch("https://meme-api.com/gimme").then(resnponse => resnponse.json()).then(data => {
+    //     // updateDetails(data.url, data.title, data.author);
+    //     console.log("URL Is ", data.url);
+    //     console.log("Title Is ", data.title);
+    //     console.log("Author Is ", data.author);
+    // });
+
+    const tempResponse = await fetch("https://meme-api.com/gimme");
+    console.log("Response is ", tempResponse);
+
+    const data = await tempResponse.json();
+    console.log("Data in Json is ", data);
+
+    updateDetails(data.url, data.title, data.author);
+
 };
 
 generateMemeBtn.addEventListener("click", generateMeme);
